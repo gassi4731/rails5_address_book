66b9e57db62f24406a27de9176e5dab0884a6776
yo higashida <penguin4731@gmail.com>
2020-10-14 11:06:42 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :users
+#  get 'users/index'
+#  get 'users/new'
+#  get 'users/edit'
+#  get 'users/update'
+#  get 'users/show'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
